5be9d7706bcd181b5e5061fbf239334beeeddb14
lexslusarchyk <lexslusarchyk@gmail.com>
2019-11-12 10:38:11 +0200

    add environment variable

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
-  apiAddress: 'http://www.api.spa-delight.lviv.ua/public/api',
-  uploadsImgAddress: 'http://www.api.spa-delight.lviv.ua/public/uploads',
+  apiAddress: 'http://api.spa-delight.lviv.ua/public/api',
+  uploadsImgAddress: 'http://api.spa-delight.lviv.ua/public/uploads',
   // apiAddress: 'api/',
   // uploadsImgAddress: 'http://localhost:8000/public/uploads',
 };

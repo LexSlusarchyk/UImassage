5115badca5551ed82d343ebc589ef5a1031f4b77
lexslusarchyk <lexslusarchyk@gmail.com>
2019-12-01 16:24:41 +0200

    add scrollToTop on sideNav opened

--- src/app/procedures/procedures.component.ts
+++ src/app/procedures/procedures.component.ts
@@ -57,5 +57,18 @@ export class ProceduresComponent implements OnInit {
 
   toggleSideNav(): void {
     this.sideNavOpened = !this.sideNavOpened;
+    this.scrollToTop();
+  }
+
+  scrollToTop(): void {
+    if (!this.sideNavOpened) { return; }
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
   }
 }
